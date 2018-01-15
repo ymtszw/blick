520de7c80ab6c3d8228008e0158d5615fb76982c
Yu Matsuzawa <yu.matsuzawa@access-company.com>
2018-01-15 18:52:47 +0900

    [master] More cleanup

--- lib/gettext.ex
+++
@@ -1,9 +0,0 @@
-use Croma
-
-defmodule Blick.Gettext do
-  use SolomonLib.Gettext, otp_app: :blick
-
-  defun put_locale(locale :: v[String.t]) :: nil do
-    Gettext.put_locale(__MODULE__, locale)
-  end
-end
--- test/blick_test.exs
+++ test/blick_test.exs
@@ -1,12 +1,12 @@
 defmodule BlickTest do
   use ExUnit.Case
 
-  test "hello should render HAML template as HTML" do
-    response = Req.get("/hello", %{}, [params: %{"locale" => "ja"}])
+  test "GET / should render HAML template as HTML" do
+    response = Req.get("/")
     assert response.status == 200
     assert response.headers["content-type"] == "text/html"
     body = response.body
     assert String.starts_with?(body, "<!DOCTYPE html>")
-    assert String.contains?(body, "Message from blick: こんにちは")
+    assert String.contains?(body, "Blick")
   end
 end
