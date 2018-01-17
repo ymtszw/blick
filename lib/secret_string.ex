a2958506284254adbf14f46ef9f162c46e4eb0a6
ymtszw <ymtszw@gmail.com>
2018-01-17 17:26:12 +0900

    [master] Hide secret value of SecretString on inspect

--- lib/secret_string.ex
+++ lib/secret_string.ex
@@ -8,28 +8,33 @@ defmodule Blick.PrintableString do
 end
 
 defmodule Blick.SecretString do
-  @moduledoc """
-  Struct module representing secret information in `Blick.PrintableString`,
-  which will always be encrypted on JSON serialization, and decrypted on deserialization.
+  @moduledoc false
 
-  ## How it works
-
-  `Poison.Encoder` protocol can be implemented for struct module,
-  (as long as it is prefixed by "YourGear.", otherwise solomon_static_analysis will bark on),
-  which dictates how that struct value will be serialized on `Poison.encode/1`.
-
-  `#{inspect(__MODULE__)}` utilizes that feature, ensuring secret information will not leak out of ErlangVM
-  by encrypting `String` value on serialization (using AES cryptosystem).
-  """
+  # Struct module representing secret information in `PrintableString`,
+  # which will always be encrypted on JSON serialization, and decrypted on deserialization.
+  #
+  # ## How it works
+  #
+  # `Poison.Encoder` protocol can be implemented for struct module,
+  # (as long as it is prefixed by "YourGear.", otherwise solomon_static_analysis will bark on),
+  # which dictates how that struct value will be serialized on `Poison.encode/1`.
+  #
+  # `SecretString` utilizes that feature, ensuring secret information will not leak out of solomon ErlangVM
+  # by always encrypting `PrintableString` value on serialization (using AES cryptosystem).
+  #
+  # It also implement `Inspect` protocol so that inspecting the struct will not expose its value.
+  # This also applies to echo-back on iex.
+  # Though directly extracting value of `:value` field WILL result in exposure of it.
+  # This should be considered "the last resort" for debugging secret information
+  # by those who know implementation details.
 
   use Croma.Struct, fields: [
     value: Blick.PrintableString,
   ]
-  alias Croma.Result, as: R
 
   defun new(any :: any) :: Croma.Result.t(t) do
     str when is_binary(str) ->
-      str |> Blick.decrypt_base64() |> R.map(&%__MODULE__{value: &1})
+      str |> Blick.decrypt_base64() |> Croma.Result.map(&%__MODULE__{value: &1})
     otherwise ->
       super(otherwise)
   end
@@ -38,5 +43,11 @@ end
 defimpl Poison.Encoder, for: Blick.SecretString do
   def encode(%Blick.SecretString{value: raw_value}, _opts) do
     ~s("#{Blick.encrypt_base64!(raw_value)}")
+  end
+end
+
+defimpl Inspect, for: Blick.SecretString do
+  def inspect(%Blick.SecretString{value: _raw_value}, _opts) do
+    "<#SecretString>"
   end
 end
