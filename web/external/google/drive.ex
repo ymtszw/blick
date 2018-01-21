be067dfc82b60efce97be343a231e8be5eb3fb98
ymtszw <ymtszw@gmail.com>
2018-01-22 01:46:31 +0900

    [master] Add Drive API module

---
+++ web/external/google/drive.ex
@@ -0,0 +1,19 @@
+use Croma
+
+defmodule Blick.External.Google.Drive do
+  @moduledoc """
+  Binding for Google Drive API.
+  """
+
+  alias Blick.External.Google
+
+  defmodule Files do
+    @base_url "https://www.googleapis.com/drive/v3"
+
+    defun get(id :: v[String.t], token :: Google.token_t) :: Google.res_t do
+      Google.request(token, :get, @base_url <> "/files/#{id}", "", %{}, params: %{"fields" => "id,name,mimeType,createdTime,owners"})
+    end
+
+    Croma.Result.define_bang_version_of(get: 2)
+  end
+end
