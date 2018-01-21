edab7dd0aef4a6b82261e96fff74ea32de9898fa
ymtszw <ymtszw@gmail.com>
2018-01-22 01:51:51 +0900

    [master] Normalize retrieved URLs

--- web/async_job/material_collecter.ex
+++ web/async_job/material_collecter.ex
@@ -3,11 +3,14 @@ use Croma
 defmodule Blick.AsyncJob.MaterialCollecter do
   alias Croma.Result, as: R
   alias Blick.External.Google.Spreadsheets
+  alias Blick.Model.Material
+
+  @type material_t :: {Material.Type.t, SolomonLib.Url.t, String.t}
 
   @rnd_seminar_spreadsheet_id "1j-ag_0n1CyLAjOTNA5bVYuCN4uq-UbFKYavU4dvh1G8"
   def rnd_seminar_spreadsheet_id(), do: @rnd_seminar_spreadsheet_id
 
-  def collect_material_urls_from_spreadsheet(token) do
+  defun collect_material_urls_from_spreadsheet(token :: Blick.External.Google.token_t) :: R.t([material_t]) do
     R.m do
       file <- Spreadsheets.get(@rnd_seminar_spreadsheet_id, token) # This can take a few seconds
       schedule_sheets <- get_shedule_sheets(file["sheets"] || [])
@@ -23,14 +26,13 @@ defmodule Blick.AsyncJob.MaterialCollecter do
   end
 
   defp parse_schedule_sheets(schedule_sheets) do
-    schedule_sheets |> Enum.flat_map(&parse_sheet/1) |> take_sample()
+    schedule_sheets |> Enum.flat_map(&parse_sheet/1) |> uniq_materials()
   end
 
-  if SolomonLib.Env.compiling_for_cloud?() do
-    defp take_sample(results), do: results
-  else
-    @sample_count 20
-    defp take_sample(results), do: results |> Enum.shuffle() |> Enum.take(@sample_count)
+  defp uniq_materials(results) do
+    results
+    |> Enum.uniq_by(fn {_type,  normalized_url, _title} -> normalized_url end)
+    |> Enum.uniq_by(fn {_type, _normalized_url,  title} -> title end)
   end
 
   defp parse_sheet(%{"data" => grid_data_list}) do
@@ -41,15 +43,20 @@ defmodule Blick.AsyncJob.MaterialCollecter do
     Enum.flat_map(row_data_list, &parse_row_data/1)
   end
 
-  @excluded_urls [
-    "https://drive.google.com/a/access-company.com/?tab=oo#folders/0B6DpgpRl_A1mfl9VblV6Rlk0TTM2YmlmYVYtUDE0VWo3ZmZ0b3BMSk55clRnYXMwN3h4N2c", # Material directory URL
+  @excluded_patterns [
+    "0B6DpgpRl_A1mfl9VblV6Rlk0TTM2YmlmYVYtUDE0VWo3ZmZ0b3BMSk55clRnYXMwN3h4N2c", # RnD seminar material directory ID
   ]
 
-  defp parse_row_data(%{"values" => cell_data_list}) do
+  defunp parse_row_data(%{"values" => cell_data_list} :: map) :: material_t do
     Enum.reduce(cell_data_list, [], fn cell, acc ->
       case cell["hyperlink"] do
-        "http" <> _ = url when not url in @excluded_urls ->
-          [{url, cell["formattedValue"] || url} | acc]
+        "http" <> _ = url ->
+          if not Enum.any?(@excluded_patterns, &String.contains?(url, &1)) do
+            {type, normalized_url} = Material.normalize_url_by_types(url)
+            [{type, normalized_url, cell["formattedValue"] || normalized_url} | acc]
+          else
+            acc
+          end
         _ ->
           acc
       end
