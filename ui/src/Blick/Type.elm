a29dea7c6d5f3f881e5001761836223b895ca220
aYuMatsuzawa <yu.matsuzawa@access-company.com>
2018-01-27 14:58:54 +0900

    [master] Implement types and decoders

--- ui/src/Blick/Type.elm
+++ ui/src/Blick/Type.elm
@@ -1,6 +1,21 @@
-module Blick.Type exposing (Flags, Model, Msg(..))
+module Blick.Type
+    exposing
+        ( Flags
+        , Model
+        , Msg(..)
+        , Material
+        , Url
+        , Email
+        , Type_(..)
+        , materialDecoder
+        )
 
 import Date exposing (Date)
+import Regex
+import Json.Decode as D exposing (Decoder)
+import Json.Decode.Extra exposing ((|:), date)
+import Json.Encode as E exposing (Value)
+import String.Extra exposing (underscored)
 
 
 -- FLAGS
@@ -39,18 +54,90 @@ type alias Material =
     }
 
 
+materialDecoder : Decoder Material
+materialDecoder =
+    D.succeed Material
+        |: D.field "title" D.string
+        |: D.field "url" urlDecoder
+        |: D.field "thumbnail_url" (D.maybe urlDecoder)
+        |: D.field "created_time" (D.maybe date)
+        |: D.field "author_email" (D.maybe emailDecoder)
+        |: D.field "type" typeDecoder
+        |: D.field "excluded" D.bool
+        |: D.field "exclude_reason" (D.maybe D.string)
+
+
 type Url
     = Url String
+
+
+urlDecoder : Decoder Url
+urlDecoder =
+    D.map Url D.string
+
+
+urlEncoder : Url -> Value
+urlEncoder (Url url) =
+    E.string url
 
 
 type Email
     = Email String
 
 
+emailDecoder : Decoder Email
+emailDecoder =
+    D.map Email D.string
+
+
+emailEncoder : Email -> Value
+emailEncoder (Email email) =
+    E.string email
+
+
 type Type_
-    = GgoogleSlide
+    = GoogleSlide
     | GoogleDoc
     | GoogleFile
     | GoogleFolder
     | Qiita
     | Html_
+
+
+typeDecoder : Decoder Type_
+typeDecoder =
+    D.map typeFromString D.string
+
+
+typeFromString : String -> Type_
+typeFromString str =
+    case str of
+        "google_slide" ->
+            GoogleSlide
+
+        "google_doc" ->
+            GoogleDoc
+
+        "google_file" ->
+            GoogleFile
+
+        "google_folder" ->
+            GoogleFolder
+
+        "qiita" ->
+            Qiita
+
+        "html" ->
+            Html_
+
+        _ ->
+            Debug.crash "Unexpected type from server!!"
+
+
+typeEncoder : Type_ -> Value
+typeEncoder type_ =
+    type_
+        |> toString
+        |> underscored
+        |> Regex.replace (Regex.AtMost 1) (Regex.regex "_$") (always "")
+        |> E.string
