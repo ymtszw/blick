62da7ef104a607440c321b9e8995a829e7950c46
aYuMatsuzawa <yu.matsuzawa@access-company.com>
2018-02-16 04:59:14 +0900

    [screenshot] Update tests

--- test/web/controller/screenshot_test.exs
+++ test/web/controller/screenshot_test.exs
@@ -9,15 +9,15 @@ defmodule Blick.Controller.ScreenshotTest do
 
   describe "All APIs" do
     test "should reject request without proper worker key" do
-      assert %{status: 401} = Req.get("/api/screenshots/new")
-      assert %{status: 401} = Req.get("/api/screenshots/new", %{"authorization" => ""})
-      assert %{status: 401} = Req.get("/api/screenshots/new", %{"authorization" => "randomstring"})
+      assert %{status: 401} = Req.get("/api/screenshots")
+      assert %{status: 401} = Req.get("/api/screenshots", %{"authorization" => ""})
+      assert %{status: 401} = Req.get("/api/screenshots", %{"authorization" => "randomstring"})
     end
   end
 
-  test "GET /api/screenshots/new should accept request with proper worker key", %{api_key: ak} do
+  test "GET /api/screenshots should accept request with proper worker key", %{api_key: ak} do
     # Should hit StubDatastore
-    assert %{status: 200, body: body} = Req.get("/api/screenshots/new", %{"authorization" => ak})
+    assert %{status: 200, body: body} = Req.get("/api/screenshots", %{"authorization" => ak})
     assert body == Poison.encode!(%{materials: []})
   end
 end
