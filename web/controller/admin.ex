32694d742823855305a51e1396124bfc6100affa
aYuMatsuzawa <yu.matsuzawa@access-company.com>
2018-03-11 05:55:06 +0900

    [master] Add directory API readonly scope

--- web/controller/admin.ex
+++ web/controller/admin.ex
@@ -34,6 +34,7 @@ defmodule Blick.Controller.Admin do
           "https://www.googleapis.com/auth/spreadsheets.readonly",
           "https://www.googleapis.com/auth/presentations.readonly",
           "https://www.googleapis.com/auth/contacts.readonly",
+          "https://www.googleapis.com/auth/admin.directory.user.readonly",
         ] |> Enum.join(" "),
       state:
         start_time
